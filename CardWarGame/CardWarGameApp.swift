940d8fdcb7e12f2c86f26b49afe2097f83b765e0
jacqueline294 <jngigi86@gmail.com>
2024-09-27 07:25:56 +0200

    Initial Commit

---
+++ CardWarGame/CardWarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardWarGameApp.swift
+//  CardWarGame
+//
+//  Created by jacqueline Ngigi on 2024-09-27.
+//
+
+import SwiftUI
+
+@main
+struct CardWarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardWarGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardWarGame
+//
+//  Created by jacqueline Ngigi on 2024-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
